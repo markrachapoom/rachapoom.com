cf1cee86cd33849422c33ade8fab916d9b577717
Mark <markrachapoom@gmail.com>
2023-12-03 01:34:47 -0800

    Init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,179 @@
-import Image from 'next/image'
+import Image from "next/image";
+import React from "react";
+
+const socials = [
+  {
+    name: "Twitter",
+    url: "https://www.twitter.com/markrachapoom",
+  },
+  {
+    name: "Github",
+    url: "https://www.github.com/markrachapoom",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/markrachapoom",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/markrachapoom",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="relative flex flex-col min-h-screen items-center bg-black">
+      {/* sticky header */}
+      <div className="flex sticky top-0 z-50 items-center justify-between w-full p-3 border-b border-dark-divider bg-[rgb(13,13,13,0.8)] backdrop-blur-xl">
+        <div className="flex flex-row items-center gap-3">
+          <img
+            src="https://pbs.twimg.com/profile_images/1669924632881942529/uVVtVMaK_400x400.jpg"
+            alt="banner"
+            className="w-7 h-7 rounded-full"
+          />
+          <span className="max-sm:hidden">Mark Rachapoom</span>
+        </div>
+
+        <div className="flex flex-row items-center justify-center gap-4">
+          {socials.map((social) => {
+            return (
+              <a href={social.url} target="_blank" key={social.name}>
+                <span className="max-sm:text-sm">{social.name}</span>
+              </a>
+            );
+          })}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* content */}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="flex flex-col w-full p-4 max-w-4xl z-0 h-auto gap-24 py-12">
+        <div className="flex flex-col items-start gap-12">
+          <div className="flex flex-col items-start gap-4">
+            <h1 className="text-4xl font-medium text-center font-serif">
+              Mark Rachapoom
+            </h1>
+            <p>
+              Hey there, I&apos;m Mark Rachapoom, a 21-year-old college dropout
+              and former software engineer for a startup in Silicon Valley who
+              can design and build iOS, Website, Full-Stack apps.
+            </p>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* <div className="flex flex-row items-center justify-between gap-4 w-full">
+              {socials.map((social) => {
+                return (
+                  <a href={social.url} target="_blank" key={social.name} className="flex items-center justify-center w-full rounded-full border p-2 border-dark-divider hover:border-dark-dividerContrast transition-all">
+                    <span className="">{social.name}</span>
+                  </a>
+                );
+              })}
+            </div> */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className="grid grid-cols-2 gap-4 w-full h-screen">
+            <div className="grid grid-rows-2 gap-4 w-full h-screen">
+              <ImageCard
+                src="mark-naval.jpeg"
+                alt="Mark & Naval"
+                caption="Taking a walk with Naval Ravikant for 2 hours"
+              />
+              <ImageCard
+                src="/nonce-kumar-mark.jpeg"
+                alt="Mark & Naval"
+                caption="Seoul, South Korea"
+              />
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="grid grid-rows-3 gap-4 w-full h-screen">
+              <ImageCard
+                src="/talent-protocol-stage.jpeg"
+                alt="Mark & Naval"
+                caption="Paris, France"
+              />
+              <ImageCard
+                src="https://pbs.twimg.com/media/F_6MTEFaAAAlSLu?format=jpg&name=4096x4096"
+                alt="Mark & Naval"
+                caption="Stinson Beach, California"
+              />
+              <ImageCard
+                src="/nonce.jpeg"
+                alt="Mark & Naval"
+                caption="Nonce Community, South Korea"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-start gap-12">
+          <h1 className="text-4xl font-medium text-center font-serif underline underline-offset-8 decoration-dark-dividerContrast">
+            Projects
+          </h1>
+
+          <div className="flex flex-col items-start gap-6">
+            <div className="flex flex-col items-start gap-3">
+
+              <span className="text-2xl font-medium leading-tight">Merse</span>
+
+              <p className="text-dark-text-secondary">
+                We&apos;re crafting tools that transform your everyday moments,
+                stories, experiences into lasting legacies in every form —
+                comics, books, films, voice recordings, autobiographies, and
+                beyond.
+              </p>
+            </div>
+
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/RSO1GCPq8r8?si=nO3mU2n0n6rfjfiz"
+              title="YouTube video player"
+              // frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              // allowfullscreen
+              className="w-full aspect-video h-auto border border-dark-divider rounded-2xl"
+            ></iframe>
+          </div>
+
+          {/* Project/Quickpitch */}
+          <div className="flex flex-col items-start gap-6">
+            <div className="flex flex-col items-start gap-3">
+              <span className="text-2xl font-medium leading-tight">Quickpitch</span>
+
+              <p className="text-dark-text-secondary">
+                Demo Day In Your Pocket: Discover startups on a Tinder-style platform: founders pitch in 15 seconds, refine through feedback, and connect with VCs.
+              </p>
+            </div>
+
+            <img
+              src="/quickpitch.png"
+              alt="quickpitch screenshots"
+              className="w-full aspect-video h-auto border border-dark-divider rounded-2xl"
+            />
+
+          </div>
+
+        </div>
       </div>
     </main>
-  )
+  );
 }
+
+const ImageCard: React.FC<{
+  src: string;
+  alt: string;
+  caption: string;
+}> = ({ src, alt, caption }) => {
+  return (
+    <div className="relative flex flex-col border-dark-divider h-full overflow-clip group">
+      <img
+        src={src}
+        alt={alt}
+        className="w-full h-full object-cover filter grayscale transition-all duration-500 ease-in-out"
+      />
+
+      <span className="py-2 absolute bottom-0 bg-black text-dark-text-secondary text-sm w-full">
+        {caption}
+      </span>
+    </div>
+  );
+};
